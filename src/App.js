9a0959359945fee670d4b5ccaafb3afd775f2458
EricMad <ermadou@gmail.com>
2022-11-23 13:51:39 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Character from "./components/Character";
-import Search from "./components/Search";
-import Quote from "./components/Quote";
-import Footer from "./components/Footer";
+import GamePlay from "./components/Gameplay"
+import Home from "./components/Home";
 import "./App.css";
-import TopLogo from "./components/TopLogo";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-const initialQuote = {
-  text: "Quote",
-  author: "Author",
-};
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      setIsLoading(true);
-      const result = await axios(
-        `https://www.breakingbadapi.com/api/characters?name=${query}`
-      );
-
-      console.log(result.data);
-
-      setItems(result.data);
-      setIsLoading(false);
-    };
-
-    fetchItems();
-  }, [query]);
-
-  const searchFunction = (el) => {
-    setQuery(el);
-  };
-
-  const [quote, setQuote] = useState(initialQuote);
-
-  const updateQuote = async () => {
-    const url = "https://www.breakingbadapi.com/api/quote/random";
-    const res = await fetch(url);
-    const [newQuote] = await res.json();
-
-    const { quote: text, author } = newQuote;
-
-    setQuote({
-      text,
-      author,
-    });
-  };
-
-  useEffect(() => {
-    updateQuote();
-  }, []);
-
+  
   return (
-    <div className="main">
-      <TopLogo />
-      <div className="quote">
-        <h3>Quotes and Characters</h3>
-        <Quote quote={quote} />
-        <button onClick={() => updateQuote()}>Random Quote</button>
-      </div>
-      <Search getQuery={searchFunction} />
-      <Character isLoading={isLoading} items={items} />
-      <Footer />
+    
+    <div className="app">
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/gameplay" element={<GamePlay />} />
+      </Routes>
+    </Router>
     </div>
+    
   );
 };
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GamePlay from './Gameplay'
+import "./Home.css"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className='home'>
+        <button><Link to="/gameplay"> Test me .. I'm a danger!</Link></button>
+    </div>
+  )
+}
+
+export default Home
